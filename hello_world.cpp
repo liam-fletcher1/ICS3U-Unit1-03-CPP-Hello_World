af7997fb49ce9288617a5e09eadbdde333d13abf
Ubuntu <ubuntu@ip-172-31-70-134.ec2.internal>
2021-09-10 19:42:28 +0000

    added hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Liam Fletcher All rights reserved
+//
+// Created by: Liam Fletcher
+// Created on: Sept 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+}
